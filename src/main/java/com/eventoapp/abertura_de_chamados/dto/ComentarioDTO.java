2eb4e5ecbab6e9ac35581a2420f1c40aa6dbeb2a
jose marcos <marcosads2025@gmail.com>
2026-06-11 13:55:02 -0300

    correção na class ComentarioDTO

--- src/main/java/com/eventoapp/abertura_de_chamados/dto/ComentarioDTO.java
+++ src/main/java/com/eventoapp/abertura_de_chamados/dto/ComentarioDTO.java
@@ -1,7 +1,26 @@
 package com.eventoapp.abertura_de_chamados.dto;
 
+import lombok.Data;
+
+@Data
 public class ComentarioDTO {
 
     private String texto;
     private Long usuarioId;
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
 }
