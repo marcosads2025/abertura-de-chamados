6cc69e18726bae31f4ee9152ba959d8215aeb6ea
jose marcos <marcosads2025@gmail.com>
2026-06-11 13:55:48 -0300

    correção na entidade comentario

--- src/main/java/com/eventoapp/abertura_de_chamados/entity/Comentario.java
+++ src/main/java/com/eventoapp/abertura_de_chamados/entity/Comentario.java
@@ -1,13 +1,20 @@
 package com.eventoapp.abertura_de_chamados.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 
 @Entity
 @Table(name = "comentarios")
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comentario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,5 +58,11 @@ public class Comentario {
 
     public void setChamado(Chamado chamado) {
         this.chamado = chamado;
+    }
+
+    public void setUsuario(Usuario usuario) {
+    }
+
+    public void setDataComentario(LocalDateTime now) {
     }
 }
